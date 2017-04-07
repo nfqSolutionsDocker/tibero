b7729ee6422bf028c9552c0dd828d3c1c9786a64
manuel <miglesiassarrai@gmail.com>
2017-04-07 15:00:49 +0200

    update

--- initial.py
+++ initial.py
@@ -1,5 +1,22 @@
-from subprocess import call
+import urllib
+import os.path
+import os
 
-url = "https://github.com/miglesiassarria/tibero/tree/master/tibero"
-for i in range(5):
-    call("curl -s --retry 3 -m 60 -o /$part -L $ur{}".format(i+1))
+url = "https://github.com/miglesiassarria/tibero/raw/master/installation/Tib6.bin.00"
+destino = "tibero/Tib6.bin.00"
+existe = "tibero/tibero6"
+
+if os.path.exists(existe):
+    print("Tibero ya se encuntra instalado")
+
+else:
+    print("INSTALANDO TIBERO")
+    for i in range(5):
+        urllib.urlretrieve(url + str(i+1), filename=destino + str(i+1))
+
+
+    os.system("cat /tibero/*.bin* > /tibero/installer")
+
+
+
+# os.system("wget --no-check-certificate --content-disposition https://github.com/miglesiassarria/tibero/raw/master/installation/Tib6.bin.001 -O tibero/Tib6.bin.001")
