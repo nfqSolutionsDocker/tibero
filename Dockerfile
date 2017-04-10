4725c368c7ccc0534e51c227d96bb2b09031d31e
manuel <miglesiassarrai@gmail.com>
2017-04-10 17:23:02 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,19 @@ FROM nfqsolutions/centos:7
 MAINTAINER solutions@nfq.com
 
 # Instalacion de paquetes
-RUN yum install -y java-1.7.0-openjdk
+RUN yum install -y java-1.7.0-openjdk && \
+    mkdir /solutions/scripts
 
 # Arreglo del error del semaforo
-RUN sysctl -w kernel.sem="10000 32000 10000 10000"
+#RUN sysctl -w kernel.sem="10000 32000 10000 10000"
+
+ADD initial.py /solutions/
+ADD installvariables.properties /solutions/
+ADD entry.sh /solutions/
+ADD scripts/* /solutions/scripts/
+
+RUN chmod +x /solutions/initial.py && \
+    chmod +x /solutions/entry.sh && \
+    chmod 777 -R /solutions/scripts
 
 CMD ["/usr/bin/supervisord"]
--- initial.py
+++ initial.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python2
+
 import urllib
-import os.path
 import os
 
 url = "https://github.com/miglesiassarria/tibero/raw/master/installation/Tib6.bin.00"
-destino = "tibero/Tib6.bin.00"
-existe = "tibero/tibero6"
+ruta_base = "/tibero"
+destino = ruta_base + "/Tib6.bin.00"
+existe = ruta_base + "/tibero6"
+instalador = ruta_base + "/instalador.bin"
 
 if os.path.exists(existe):
     print("Tibero ya se encuntra instalado")
@@ -14,9 +17,11 @@ else:
     for i in range(5):
         urllib.urlretrieve(url + str(i+1), filename=destino + str(i+1))
 
+    os.system("cat " + destino + "* > " + ruta_base + "/installer.bin")
+    os.system("chmod +x " + ruta_base + "/instalador.bin")
+    os.system(instalador + " -f /solutions/installvariables.properties -i silent")
+    os.system("chmod +x /tibero/tibero6/client/bin/install.sh")
+    print("Instalacion de TIBERO 6 completada")
 
-    os.system("cat /tibero/*.bin* > /tibero/installer")
-
-
-
-# os.system("wget --no-check-certificate --content-disposition https://github.com/miglesiassarria/tibero/raw/master/installation/Tib6.bin.001 -O tibero/Tib6.bin.001")
+print("Preparando scripts de base de datos")
+os.system("cp /solutions/scripts/* /tibero/")
